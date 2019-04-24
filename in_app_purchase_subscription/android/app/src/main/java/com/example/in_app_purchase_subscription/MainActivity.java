1c966f72cfad602b7c668b74e2dc54478c5614c0
awaik <awaik@live.com>
2019-04-24 19:53:36 +0300

    added example for in app purchase subscription

---
+++ in_app_purchase_subscription/android/app/src/main/java/com/example/in_app_purchase_subscription/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.in_app_purchase_subscription;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
